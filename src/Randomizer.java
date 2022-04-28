49be65b2f45c834c9b9ff6dd7fba6a0ae8cb21c1
Tyler C <trchang@asu.edu>
2022-04-28 11:01:32 -0700

    adjusted Randomizer to fit canvas size

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -2,15 +2,14 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Randomizer {
-    public static ArrayList<Dot> randomize(){
-        System.out.println("In Randomizer");
+    public static ArrayList<Dot> randomize(int x, int y){
         ArrayList<Dot> dotList = new ArrayList<>();
         int randX;
         int randY;
         for (int i = 0; i <100; i++){
             Random rand = new Random();
-            randX = rand.nextInt(500);
-            randY = rand.nextInt(500);
+            randX = rand.nextInt(x);
+            randY = rand.nextInt(y);
             dotList.add(new Dot(randX, randY));
         }
         return dotList;
